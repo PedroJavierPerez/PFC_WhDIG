a964b2378783ebe968cd819c4f3264056557b5a9
PedroJavierPerez <perico-20@hotmail.com>
2014-09-05 23:20:32 +0200

    html primera página

--- WhDIG/index.php
+++ WhDIG/index.php
@@ -1,20 +1,198 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
+
+<!--To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+and open the template in the editor.-->
+
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Empezamos ya</title>
+        <title>WhDIG</title>
+        <link rel="stylesheet" type="text/css" href="css/estilos.css">  
     </head>
     <body>
-        eiiiii
-        Brother
-        Pues parece que si
+        <header>
+            <div id="subheader">
+                <div id="logo"><p><a href="">WhDIG</a></p></div>
+                <div id="logo2"><h2>Where do I go?</h2></div>
+                <div id="identificarse">
+                    <form>
+                        <label for ="email">Email:</label>
+                        <input type="email" id="email" placeholder="Escribe tu email" required>
+                        <label for ="contrasena">Contraseña:</label>
+                        <input type="password" id="contrasena" placeholder="Escribe tu contraseña" required>
+                        <input type="submit" value="Entrar" id="btnEntrar">
+                        
+                            
+                    </form>
+                    <div id="olvidarContraseña">
+                    <p><a href="">¿Has olvidado tu contraseña?</a></p>
+                    </div>
+                </div>
+            </div>
+       
+        </header>
+        
+        <section id="wrap">
+            <section id ="main">
+                <section id="bienvenidos">
+                    <article>
+                        <hgroup><h3>Bienvenido a nuestra web de eventos</h3></hgroup>
+                    </article>
+                </section>
+                <section id="eventos">
+                    <article>
+                        <hgroup><h4 class="titulo">Cerveza a 1€ (Jaén)</h4></hgroup>
+                        <p>
+                        <ul>
+                            <li>Jueves cerveza a 1€ desdelas 10:00 a las 12:00 de la noche.</li>
+                            <li>Fecha: 05/09/2014</li>
+                            <li>Hora: 22:00</li>
+                        </ul>
+                        </p>
+                    </article>
+                    
+                     <article>
+                        <hgroup><h4 class="titulo">Cerveza a 1€ (Jaén)</h4></hgroup>
+                        <p>
+                        <ul>
+                            <li>Jueves cerveza a 1€ desdelas 10:00 a las 12:00 de la noche.</li>
+                            <li>Fecha: 05/09/2014</li>
+                            <li>Hora: 22:00</li>
+                        </ul>
+                        </p>
+                    </article>
+                    
+                     <article>
+                        <hgroup><h4 class="titulo">Cerveza a 1€ (Jaén)</h4></hgroup>
+                        <p>
+                        <ul>
+                            <li>Jueves cerveza a 1€ desdelas 10:00 a las 12:00 de la noche.</li>
+                            <li>Fecha: 05/09/2014</li>
+                            <li>Hora: 22:00</li>
+                        </ul>
+                        </p>
+                    </article>
+                </section>
+                
+                <aside>
+                    <section id="filtro">
+                        <hgroup><h5>Eventos de hoy:</h5></hgroup>
+                        <ul>
+                            <li><a href="">22:00 - Cerveza 1€</a></li>
+                            <li><a href="">18:00 - Curso de cocina</a></li>
+                        </ul>
+                    </section>
+                    
+                    <section id="filtro">
+                        <hgroup><h5>Suscribete:</h5></hgroup>
+                        <div id="informacion">
+                            <form>
+                                <label><label for ="Iemail">Email:</label>
+                                <input type="email" id="Iemail" placeholder="Escribe tu email" required>
+                                <input type="submit" value="Recibir" id="btnRecibir">
+                                <input type="submit" value="No Recibir" id="btnNoRecibir">
+                            </form>
+                            <p>
+                                Introduzca su email para recibir información de eventos o para dejar de recibirla.
+                            </p>
+                                
+                        </div>
+                        
+                    </section>
+                    
+                </aside>
+            </section>
+            <footer>
+                <section id="acerca-de">
+                    <article>
+                        <hgroup><h3>Acerca de...</h3></hgroup>
+                        <p>lkndsfgkhsfdkjghskfdljgblkjsfdbgkdgdlkfgbdfkjg
+                        fghgfhgfhgfhdgfhfghdfghghgfhgfdhdghghghgfhdfhdgfhg
+                        dfghdhghgh</p>
+                    </article>
+                </section>
+                
+                <section id="redes-s">
+                    <div class="email"><a href="#"></a></div>
+                    <div class="twitter"><a href="#"></a></div>
+                    <div class="youtube"><a href="#"></a></div>
+                    <div class="facebook"><a href="#"></a></div>
+                </section>
+ 
+            </footer>
+            <div id="copyright"><p>This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit 2012 - 2013.</p></div>
+        </section>
+               
+        
         <?php
         // put your code here
         ?>
     </body>
 </html>
+<!--<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" href="css/estilos.css" type="text/css">
+    <title>Practica en tu Blog</title>
+  </head>
+  <body>
+    <header id="page_header">
+      <h1>Mi Blog!</h1>
+      <nav>
+        <ul>
+          <li><a href="http://www.google.com">Ir a Google</a></li>
+          <li><a href="http://www.yahoo.com">Ir a Yahoo</a></li>
+          <li><a href="http://www.altavista.com">Ir a altavista</a></li>
+        </ul>
+      </nav>
+    </header>
+    <section id="posts">
+      <article class="post">
+      <header>
+        <h2>Que es un motor de busqueda o buscador en Internet?</h2>
+        <p>Posted by Pepe on
+<time datetime="2011-05-17T09:39">Mayo 17, 2011 at 9:39AM</time>
+        </p>
+      </header>
+      <aside>
+        <p> &quot;Google, Yahoo y Bing son algunos de los buscadores de Internet.&quot; </p>
+      </aside>
+      <p>
+        Un motor de busqueda es un sistema informatico que busca archivos almacenados 
+        en servidores web gracias a su spider o web crawler (restreador). Un ejemplo 
+        de estos son los buscadores de Internet que buscan informacion sobre algun tema. 
+      </p>
+      <p>
+Las busquedas se hacen con palabras clave o con arboles jerarquicos por temas, 
+el resultado de la busqueda es un listado de direcciones web en los que se mencionan
+temas relacionados con las palabras clave buscadas.</br>
+Los buscadores pueden ser clasificados en 2 tipos:</br>
+<b>Indices tematicos:</b> son sistemas de busqueda por temas o categorias jerarquizados. Son
+bases de datos de direcciones web elaboradas manualmente, es decir hay personas que asignan
+las paginas web a una categoria determinada.</br>
+<b>Motores de busqueda:</b> sistemas de busqueda por palabras clave. Incorporan a las paginas web mediante robots de busqueda en la red.
+      </p>
+      <footer>
+        <p>Esta es la parte del footer donde se puede agregar contenido.....</p>
+      </footer>
+      </article>
+    </section>
+    <section id="sidebar">
+<nav>
+        <h3>Otros lugares de consulta</h3>
+        <ul>
+          <li><a href=http://es.wikipedia.org/wiki/Wikipedia:Portada>Wikipedia</a></li>
+          <li><a href=http: //www.conocimientosweb.net/portal/encyclopedia.html>Conocimientos web</a></li>
+          <li><a href=http://enciclopedia.us.es>Enciclopedia libre universal</a></li>
+          <li><a href=http://www.webopedia.com>Webopedia</a></li>
+          <li><a href=http://www.laenciclopedia.com>La enciclopedia</a></li>
+        </ul>
+      </nav>
+    </section>
+    <footer id="page_footer">
+          <p>&copy; 2011 Derechos Reservados.</p>
+    </footer>
+  </body>
+</html>-->
